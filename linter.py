dc43d5af39cfbff716ce9b41eb2b3bbdccddc681
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-07-16 16:34:57 +1200

    Initial version designed for sqlint 0.0.3

---
+++ linter.py
@@ -0,0 +1,36 @@
+#
+# sqlint.py
+# SQL Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Steve Purcell & Kieran Trezona-le Comte
+# Copyright (c) 2015 Powershop NZ Ltd
+#
+# License: MIT
+#
+
+"""This module exports the Sqlint plugin class."""
+
+from SublimeLinter.lint import Linter, util
+
+
+class Sqlint(Linter):
+
+    """Provides an interface to sqlint."""
+
+    cmd = 'sqlint'
+    syntax = 'sql'
+    tempfile_suffix = 'sql'
+
+    version_args = '--version'
+    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>= 0.0.3'
+
+    regex = (
+        r'^.+?:(?P<line>\d+):(?P<col>\d+):'
+        r'(?:(?P<error>ERROR)|(?P<warning>WARNING)) '
+        r'(?P<message>.+$(?:\r?\n  .+$)*)'
+    )
+
+    multiline = True
+    line_col_base = (1, 1)
+    error_stream = util.STREAM_STDOUT
